43c9e0289a08e25fa1e1c3a3497f94b2f1c1a653
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-06 19:35:11 -0300

    Atribuição de variáveis para o módulo

--- environments/development/manifests/site.pp
+++ environments/development/manifests/site.pp
@@ -1,3 +1,7 @@
 node default {
-	include selenium_md_oracledb
+	class {'selenium_md_oracledb':
+		oracleHome => "/opt/oradb/u01/app/oracle/product/11.2.0/dbhome_1",
+		dbname     => "VTABOL",
+		systempass => "vtax05",
+	}
 }
