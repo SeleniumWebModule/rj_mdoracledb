471c4033315942dc204fc6e6ae25317b1af40722
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-07 03:34:09 -0300

    Remoção de código estático, passando a receber parametros do Vagrantfile que futuramente será gerado pelo Selenium Proc Control

--- environments/development/manifests/site.pp
+++ environments/development/manifests/site.pp
@@ -1,7 +1,11 @@
 node default {
 	class {'selenium_md_oracledb':
-		oracleHome => "/opt/oradb/u01/app/oracle/product/11.2.0/dbhome_1",
-		dbname     => "VTABOL",
-		systempass => "vtax05",
+	 	oracleBaseName    => $::oracleBaseName,
+		oracleVersion     => $::oracleVersion,
+		pathName          => $::pathName,
+		dbname            => $::dbname,
+		systempass        => $::systempass,
+		sid               => $::sid,
+		portdb            => $::portdb
 	}
 }
